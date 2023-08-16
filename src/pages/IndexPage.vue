b1492e804db49357f5cbf84eec520ff2cbf90279
Xenia Lu <xenia.lyy@gmail.com>
2023-08-16 09:38:04 +0800

    reversed patch: add contact info and privacy tips (bfdafc4d)

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -17,6 +17,14 @@
       }}），欢迎使用 FreeAuth！<br /><br />
       请您在使用前，先与本系统的管理员确认您所需的权限是否已经完成配置。
     </q-item-label>
+    <q-item-label class="q-px-sm q-pt-xl text-h6">
+      <q-badge align="top">体验版</q-badge>
+      试用全部功能，请发送邮件至 freeauth@decentfox.com 申请管理员权限。
+    </q-item-label>
+    <q-item-label class="q-pa-sm" header>
+      <q-icon name="announcement" color="warning" size="sm" />
+      提示：体验环境下的数据会被定期清除；同时在线体验的名额不限，且共享一份数据库，请试用者慎重填写真实信息。
+    </q-item-label>
   </page-wrapper>
 </template>
 
